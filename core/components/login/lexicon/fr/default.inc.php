e223091f76f5ed06ee32943c41ee3c65dfe35575
Thomas Jakobi <buero@treehillstudio.de>
2021-10-10 11:24:03 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: MODX Extras/MODX Login/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-login-standard/fr/

--- core/components/login/lexicon/fr/default.inc.php
+++ core/components/login/lexicon/fr/default.inc.php
@@ -27,10 +27,10 @@
  * @subpackage lexicon
  */
 $_lang['login'] = 'Connexion';
-$_lang['login.invalid_post'] = 'Login invalid POST action: [[+action]]';
-$_lang['login.login_err'] = 'Erreur inconnue lors de la connexion!';
+$_lang['login.invalid_post'] = 'WebLogin invalide POST action : [[+action]]';
+$_lang['login.login_err'] = 'Erreur inconnue lors de la connexion !';
 $_lang['login.logout'] = 'Déconnexion';
-$_lang['login.logout_err'] = 'Erreur inconnue lors de la déconnexion!';
+$_lang['login.logout_err'] = 'Erreur inconnue lors de la déconnexion !';
 $_lang['login.password'] = 'Mot de passe';
 $_lang['login.recaptcha_err_load'] = 'Impossible de charger la classe de service reCaptcha.';
 $_lang['login.username'] = 'Identifiant';
