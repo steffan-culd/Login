53caafdc463e6dccdec618f9ba2ff34153732b66
Thomas Jakobi <buero@treehillstudio.de>
2022-03-18 08:03:07 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/MODX Login/reCAPTCHA
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-login-recaptcha/da/

--- core/components/login/lexicon/da/recaptcha.inc.php
+++ core/components/login/lexicon/da/recaptcha.inc.php
@@ -32,4 +32,3 @@ $_lang['recaptcha.mailhide_no_mcrypt'] = 'For at anvende reCAPTCHA Mailhide skal
 $_lang['recaptcha.mailhide_no_api_key'] = 'For at anvende reCAPTCHA Mailhide skal du bestille en public and private key. Det kan du gøre på <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
 $_lang['recaptcha.no_api_key'] = 'For at anvende reCAPTCHA skal du bruge en API key fra <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
 $_lang['recaptcha.no_remote_ip'] = 'Af sikkerhedsmæssige årsager er du øndt til at angive remote IP til reCAPTCHA';
-
