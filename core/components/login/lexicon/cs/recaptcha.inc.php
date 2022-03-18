7c3fcb512bd083487927273ac096c0281157909a
Thomas Jakobi <buero@treehillstudio.de>
2022-03-18 08:02:58 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/MODX Login/reCAPTCHA
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-login-recaptcha/cs/

--- core/components/login/lexicon/cs/recaptcha.inc.php
+++ core/components/login/lexicon/cs/recaptcha.inc.php
@@ -32,4 +32,3 @@ $_lang['recaptcha.mailhide_no_mcrypt'] = 'Pro využívání reCAPTCHA Mailhide m
 $_lang['recaptcha.mailhide_no_api_key'] = 'Pro využívání reCAPTCHA Mailhide se musíte zaregistrovat a tak získat veřejný a soukromý klíč, můžete tak učinit na adrese <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
 $_lang['recaptcha.no_api_key'] = 'Pro využívání reCAPTCHA musíte získat klíč API na adrese <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
 $_lang['recaptcha.no_remote_ip'] = 'Z bezpečnostních důvodů, musíte zadat vzdálenou IP do reCAPTCHA';
-
