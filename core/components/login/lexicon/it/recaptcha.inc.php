4217094bfa84038e3d8cbbc66f6d749985ea6858
Thomas Jakobi <buero@treehillstudio.de>
2022-03-18 08:03:21 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/MODX Login/reCAPTCHA
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-login-recaptcha/it/

--- core/components/login/lexicon/it/recaptcha.inc.php
+++ core/components/login/lexicon/it/recaptcha.inc.php
@@ -32,4 +32,3 @@ $_lang['recaptcha.mailhide_no_mcrypt'] = 'Per utilizzare reCAPTCHA Mailhide, è 
 $_lang['recaptcha.mailhide_no_api_key'] = 'Per utilizzare reCAPTCHA Mailhide, è necessario iscriversi per ottenere la chiave privata e pubblica: <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
 $_lang['recaptcha.no_api_key'] = 'Per utilizzare reCAPTCHA è necessario ottenere una chiave API: <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
 $_lang['recaptcha.no_remote_ip'] = 'Per ragioni di sicurezza è necessario passare a reCAPTCHA l’ip remoto';
-
