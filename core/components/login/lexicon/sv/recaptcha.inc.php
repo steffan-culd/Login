bedf6241bf4c1600ea2005e1b176d9981ee9710b
Thomas Jakobi <buero@treehillstudio.de>
2022-03-18 08:05:50 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/MODX Login/reCAPTCHA
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-login-recaptcha/sv/

--- core/components/login/lexicon/sv/recaptcha.inc.php
+++ core/components/login/lexicon/sv/recaptcha.inc.php
@@ -32,4 +32,3 @@ $_lang['recaptcha.mailhide_no_mcrypt'] = 'För att använda reCAPTCHA Mailhide, 
 $_lang['recaptcha.mailhide_no_api_key'] = 'För att använda reCAPTCHA Mailhide, behöver du registrera dig för en publik och privat nyckel. Du kan göra detta på adressen <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
 $_lang['recaptcha.no_api_key'] = 'För att använda reCAPTCHA behöver du skaffa en API nyckel från <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
 $_lang['recaptcha.no_remote_ip'] = 'Av säkerhetsskäl behöver du förmedla en remote ip till reCAPTCHA';
-
